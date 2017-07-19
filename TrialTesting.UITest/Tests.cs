536a05f0a22b21b9611297e6e05098df8c5983d3
AMAIISDOM\bgarcia <BryanAnthony.Garcia@navitaire.com>
2017-07-19 21:15:53 +0800

    Add project files.

---
+++ TrialTesting.UITest/Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace TrialTesting.UITest
+{
+    [TestFixture(Platform.Android)]
+    [TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.WaitForElement("Click me first");
+            app.Screenshot("First screen.");
+            app.Tap("Click me first");
+            app.WaitForElement("Go back");
+            app.Tap("Go back");
+            app.Screenshot("Third screen.");
+        }
+    }
+}
+
